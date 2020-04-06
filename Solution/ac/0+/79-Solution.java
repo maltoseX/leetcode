1e0923040f7217d5bf0115c18f6d29becf1aad23
tostw <tostw@hxl.com>
2020-04-06 22:13:49 +0800

    solve the problem No.79

---
+++ Solution/ac/0+/79-Solution.java
@@ -0,0 +1,63 @@
+
+/**
+ * @author tostw
+ * No.79 单词搜索
+ */
+public class Solution {
+    boolean[][] marked;
+    int[][] direction = {
+            {-1, 0},
+            {0, -1},
+            {0, 1},
+            {1, 0}
+    };
+    int m;
+    int n;
+    char[][] board;
+    String word;
+
+    public boolean exist(char[][] board, String word) {
+        if (board.length == 0) {
+            return false;
+        }
+        m = board.length;
+        n = board[0].length;
+        marked = new boolean[m][n];
+        this.board = board;
+        this.word = word;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int start) {
+        if (start == word.length() - 1) {
+            return board[i][j] == word.charAt(start);
+        }
+
+        if (board[i][j] == word.charAt(start)) {
+            marked[i][j] = true;
+            for (int k = 0; k < 4; k++) {
+                int newX = i + direction[k][0];
+                int newY = j + direction[k][1];
+                if (inArea(newX, newY) && !marked[newX][newY]) {
+                    if (dfs(newX, newY, start + 1)) {
+                        return true;
+                    }
+                }
+            }
+            marked[i][j] = false;
+        }
+        return false;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+}
