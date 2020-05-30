be7c9735378e31bde8f35438ce97d371cc83e80f
tostw <tostw@hxl.com>
2020-05-30 18:15:39 +0800

    solve the problem No.867

---
+++ Solution/ac/800+/867-Solution.java
@@ -0,0 +1,20 @@
+/**
+ * @author tostw
+ * No.867 转置矩阵
+ */
+
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int m = A.length;
+        int n = A[0].length;
+        int[][] B = new int[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                B[j][i] = A[i][j];
+            }
+        }
+
+        return B;
+    }
+}
