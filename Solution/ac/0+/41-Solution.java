bfbc9ad187da4be35aadd781c048b4824eed17a2
tostw <tostw@hxl.com>
2020-06-27 12:38:13 +0800

    solve the problem No.41

---
+++ Solution/ac/0+/41-Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * @author tostw
+ * No.41 缺失的第一个正数
+ */
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return len + 1;
+    }
+}
