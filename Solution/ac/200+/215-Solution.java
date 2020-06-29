13f76ee9318377320dc37a8effbe54662440a06f
tostw <tostw@hxl.com>
2020-06-29 08:36:34 +0800

    solve the problem No.215

--- Solution/ac/200+/215-Solution.java
+++ Solution/ac/200+/215-Solution.java
@@ -6,7 +6,35 @@ import java.util.*;
  */
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        return quickSort(nums, 0, nums.length - 1, k);
+    }
+
+    private int quickSort(int[] nums, int l, int r, int k) {
+        if (l >= r) {
+            return nums[l];
+        }
+        int i = l, j = r;
+        int mid = nums[(l + r) >> 1];
+        while (i <= j) {
+            while (nums[i] < mid) {
+                i++;
+            }
+            while (nums[j] > mid) {
+                j--;
+            }
+            if (i < j) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                i++;
+                j--;
+            } else if (i == j) {
+                i++;
+            }
+        }
+        if (j < nums.length - k) {
+            return quickSort(nums, j + 1, r, k);
+        }
+        return quickSort(nums, l, j, k);
     }
 }
